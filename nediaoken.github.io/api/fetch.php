eb76784793a0661ade4c132c34aa770f60b8192c
Aiden <aslvisser@hotmail.com>
2025-05-24 18:07:48 -0700

    fetch changes

--- nediaoken.github.io/api/fetch.php
+++ nediaoken.github.io/api/fetch.php
@@ -18,16 +18,8 @@
 
     // Query all users
     $sql = "SELECT * FROM pokemon LIMIT 5";
-    $result = $conn->query($sql);
-
-    $users = [];
-    if ($result && $result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $users[] = $row;
-        }
-    }
-
-    echo json_encode($users);
+    $result = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+    echo json_encode($result);
 
     $conn->close();
 ?>
